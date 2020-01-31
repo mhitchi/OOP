008db41a5e876f9e70b20cd67039f0896e602c23
mhitchi <mayhitchings@gmail.com>
2020-01-31 11:39:41 -0500

    logging all manager info except id

--- cli.js
+++ cli.js
@@ -1,48 +1,36 @@
-//use inquirer to ask questions
-
+//** TODO:*/
+//** Make sure manager's E-mail is being generated */
+//** Create prompt for adding additional roles */
+//** Confirm constructors are creating objects properly for additional employees */
+//** Output to text file (format output so it is legible) */
 //dependencies
+//use inquirer to ask questions
 const inquirer = require("inquirer");
 const fs = require("fs");
-
 const Employee = require("./lib/employeeClass");
 const Manager = require("./lib/managerClass");
 const Intern = require("./lib/internClass");
 const Engineer = require("./lib/engineerClass");
-
 const writeFile = require("./app");
-
+//**Dan: Added this comment for me. Count for assigning ID number */
 let count = 0;
-
 //ask for manager info first
 //make new manager object
 //put manager info in txt file
-
 //ask for team member role, name, id
 //switch based on role, ask for role info
 //make intern or engineer object
 //put into txt file
 //ask if done
 //either repeat or end
-
 //ask for manager info first
 const getManager = () => {
-  try {
     inquirer.prompt([
       {
         type:"input",
         message: "Enter manager name",
         name: "name"
       },
-      // {
-      //   type:"list",
-      //   message: "Select role",
-      //   name: "role",
-      //   choices: [
-      //     "Engineer",
-      //     "Intern",
-      //     "Manager"
-      //   ]
-      // },
       {
         type:"input",
         message: "Enter email",
@@ -56,35 +44,31 @@ const getManager = () => {
     ]).then((response) => {
       //return manager info
       let name = response.name;
-      let role = "manager";
+      let role = "Manager";
+      let email = response.email;
       count++;
+      //TODO returning undefined
       let id = count;
       let officeNum = response.officeNum;
-
-      makeMan(name, role, id, officeNum);
-      
+      makeMan(name, role, id, email, officeNum);
     });
-  } catch (err) {
-    console.log("Error with inquirer");
-  }
 }
 
-getManager();
-
-const makeMan = (name, role, id, officeNum) => {
+const makeMan = (name, role, id, email, officeNum) => {
   //make new manager obj
-  const newMan = new Manager(name, role, id, officeNum);
+  const newMan = new Manager(name, role, id, email, officeNum);
+
+  console.log(newMan.name, newMan.role, newMan.id, newMan.email, newMan.officeNum)
 
   writeMan(newMan);
 }
-
+// writeFile();
 const writeMan = (newMan) => {
-  fs.appendFile("team.txt", newMan, (err) => {
+  fs.appendFile("team.txt", (`${newMan.name}, ${newMan.role}, ${newMan.id}, ${newMan.email}, ${newMan.officeNum}`), (err) => {
     if(err) {
       console.log("Error with writing file");
     }
   });
 }
-// writeFile();
-
-module.exports = ask;
+// start program and get Manager information
+getManager();
