623761ecbc9f3f28cdbfe7b10af50f6e9fe9a1f1
mhitchi <mayhitchings@gmail.com>
2020-02-02 14:32:09 -0500

    added more comments

--- cli.js
+++ cli.js
@@ -12,15 +12,7 @@ const Engineer = require("./lib/engineerClass");
 
 //**Dan: Added this comment for me. Count for assigning ID number */
 let count = 0;
-//ask for manager info first
-//make new manager object
-//put manager info in txt file
-//ask for team member title, name, id
-//switch based on title, ask for title info
-//make intern or engineer object
-//put into txt file
-//ask if done
-//either repeat or end
+
 //ask for manager info first
 const getManager = () => {
     inquirer.prompt([
@@ -46,6 +38,8 @@ const getManager = () => {
       let email = response.email;
       let id = count;
       let officeNum = response.officeNum;
+
+      //call function to make new manager obj
       makeMan(name, title, id, email, officeNum);
     });
 }
@@ -54,17 +48,14 @@ const makeMan = (name, title, id, email, officeNum) => {
   //make new manager obj
   const newMan = new Manager(name, title, id, email, officeNum);
 
-  // console.log(newMan.name, newMan.title, newMan.id, newMan.email, newMan.officeNum)
-
-  //writeMan(newMan);
   //call writeFile function from manager class
-  //TODO not working
   newMan.writeToFile()
 
+  //call function to get employee info
   getEmployee();
 }
 
-//get employees
+//get employee info
 const getEmployee = () => {
   inquirer.prompt([
     {
@@ -87,31 +78,33 @@ const getEmployee = () => {
       ]
     }
   ]).then((response) => {
-    //return manager info
+    //return employee info
     let name = response.name;
     let title = response.title;
     count++;
     let id = count;
     let email = response.email;
 
+    //call function to get info based on title
     getTitleInfo(name, title, id, email);
 
   });
 
   //get info based on title
-  //get info based on title
   const getTitleInfo = (name, title, id, email) => {
     switch(title) {
       case "engineer":
+        //if engineer, call function to ask for github
         ask(name, title, id, email, "Github url");
         break;
       case "intern":
+        //if intern, call function to ask for school
         ask(name, title, id, email, "school");
         break;
     }
   }
 
-  //engineer or intern inquirer questions
+  //ask for info based on title
   const ask = (name, title, id, email, infoType) => {
     inquirer.prompt(
       {
@@ -120,14 +113,13 @@ const getEmployee = () => {
       name: "titleInfo"
       }).then((response) => {
         let titleInfo = response.titleInfo;
-        //TODO add shit to txt file
 
-        //console.log (name, title, id, email, titleInfo);
+        //call function to ask if the user is finished building their team
         askIfDone(name, title, id, email, titleInfo);
     });
   }
 
-    //ask if they're done building the team yet
+  //ask if the user is finished building their team
   const askIfDone = (name, title, id, email, titleInfo) => {
     inquirer.prompt(
       {
@@ -141,6 +133,7 @@ const getEmployee = () => {
       }).then((response) => {
         const finished = response.finished;
 
+        //if not finished, repeat getEmployee process
         if( finished === "No" ) {
           console.log("Keep building your team");
           getEmployee();
@@ -148,6 +141,8 @@ const getEmployee = () => {
           console.log("Checkout your team in team.txt");
         }
       });
+
+    //call function to make engineer or intern
     makeMember(name, title, email, id, titleInfo)
   }
 }
@@ -158,13 +153,15 @@ const makeMember = (name, title, email, id, titleInfo) => {
 
   switch(title) {
       case "engineer":
+        //if engineer, make new engineer
         newMember = new Engineer(name, title, id, email, titleInfo)
-        //TODO not working
+        //write new engineer to team.txt
         newMember.appendToFile();
         break;
       case "intern":
+        //if intern, make new intern
         newMember = new Intern(name, title, id, email, titleInfo)
-        //TODO not working
+        //write new intern to team.txt
         newMember.appendToFile();
         break;
     }
